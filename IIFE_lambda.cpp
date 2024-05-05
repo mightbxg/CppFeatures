ef86fbc668a244ea42d5fd995893ef2b71713b76
Xinguang Bian <mightbxg@hotmail.com>
2024-05-05 11:55:42 +0800

    add more to IIFE_lambda

--- IIFE_lambda.cpp
+++ IIFE_lambda.cpp
@@ -1,6 +1,11 @@
+#include <Eigen/Core>
 #include <cmath>
 #include <iostream>
 #include <vector>
+
+// lambda 表达式可以在定义的时候立即执行，这种用法被称为“立即执行函数表达式”
+// (Immediately-invoked function expression, IIFE),
+// 能够起到复杂初始化、变量隔离、简化代码、使代码结构更清晰等作用
 
 struct OneFloat {
   float val{0.0f};
@@ -29,7 +34,7 @@ struct MyStructIIFE {
   OneFloat var{0.0f};
   // OneFloat 只有一次构造
   MyStructIIFE(bool flag1, bool flag2)
-      : var([flag1, flag2] {
+      : var([flag1, flag2] {  // 复杂初始化
           float a = flag1 ? 1.0 : 2.0;
           float b = flag2 ? 3.0 : 4.0;
           return std::pow(b, a);
@@ -60,11 +65,11 @@ void func() {
 
 void func_IIFE() {
   auto has_zero = [&]() -> bool {
-    auto temp_data = load_data();
+    auto temp_data = load_data();  // 变量隔离
     for (const auto& vss : temp_data) {
       for (auto& vs : vss) {
         for (auto v : vs) {
-          if (v == 0) return true;
+          if (v == 0) return true;  // 简化代码
         }
       }
     }
@@ -74,10 +79,14 @@ void func_IIFE() {
   some_other_function(has_zero);
 }
 
-
-
-
 int main() {
+  // 复杂初始化
+  static const Eigen::Matrix2f R = [] {
+    Eigen::Matrix2f mat;
+    mat << 0, 1, -1, 0;
+    return mat;
+  }();
+
   std::cout << "MyStruct ----------------\n";
   MyStruct s1(false, true);
   std::cout << "MyStructIIFE ------------\n";
